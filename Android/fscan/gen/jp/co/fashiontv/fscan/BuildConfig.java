4ac72b075ac06a6bac4b2468914ad5cff71581bf
Alsor Zhou <alsor.zhou@gmail.com>
2013-11-08 19:40:07 +0800

    [android] commit change

--- Android/fscan/gen/jp/co/fashiontv/fscan/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.co.fashiontv.fscan;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Android/fscan/src/jp/co/fashiontv/fscan/MethodCall.java
+++ Android/fscan/src/jp/co/fashiontv/fscan/MethodCall.java
@@ -4,24 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class MethodCall {
-	public MethodCall(String action,Activity parentActivity){
+    public MethodCall(String action, Activity parentActivity) {
 
-		Intent intent=new Intent();
-		String packageName = "jp.co.fashiontv.fscan";
-		String activityName = packageName + "." + action;
+        Intent intent = new Intent();
+        String packageName = "jp.co.fashiontv.fscan";
+        String activityName = packageName + "." + action;
 
-		Class intentClass;
-		try {
-			intentClass = Class.forName(activityName);
-			intent = new Intent(parentActivity.getApplication(), intentClass);
-			parentActivity.startActivity(intent);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        Class intentClass;
+        try {
+            intentClass = Class.forName(activityName);
+            intent = new Intent(parentActivity.getApplication(), intentClass);
+            parentActivity.startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
 //		intent.setClassName(packageName,activityName);
 //		parentActivity.startActivity(intent);
 
-	}
+    }
 }
