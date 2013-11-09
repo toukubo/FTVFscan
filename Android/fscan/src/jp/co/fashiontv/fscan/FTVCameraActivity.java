0c3e61181db2230f2d857016cd9d22b4c94dec55
Alsor Zhou <alsor.zhou@gmail.com>
2013-11-10 01:18:08 +0800

    android - open internal webview instead of external browser

--- Android/fscan/src/jp/co/fashiontv/fscan/FTVCameraActivity.java
+++ Android/fscan/src/jp/co/fashiontv/fscan/FTVCameraActivity.java
@@ -2,12 +2,17 @@ package jp.co.fashiontv.fscan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
-import jp.co.fashiontv.fscan.Utils.BitmapUtil;
+import jp.co.fashiontv.fscan.Common.StringUtil;
+import jp.co.fashiontv.fscan.ImgProc.FTVImageProcEngine;
 
 public class FTVCameraActivity extends Activity {
+    private static String TAG = "FTVCameraActivity";
+
 	final int CAMERA_RESULT = 0;
 
 	@Override
@@ -16,11 +21,9 @@ public class FTVCameraActivity extends Activity {
 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-//		setContentView(R.layout.activity_main);
-		Intent intent = new Intent();  
+		Intent intent = new Intent();
 		intent.setAction("android.media.action.IMAGE_CAPTURE"); 
 		startActivityForResult(intent, CAMERA_RESULT);
-
 	}
 
 	@Override
@@ -28,26 +31,30 @@ public class FTVCameraActivity extends Activity {
 		super.onActivityResult(requestCode, resultCode, intent);
 
 		if (requestCode == CAMERA_RESULT && resultCode == RESULT_OK) {
-			byte[] imgData = BitmapUtil.getImageBytes(intent);
+            Bitmap originImage = (Bitmap) intent.getExtras().get("data");
 
-            // TODO: resize image data
+            // resize image data
+            Bitmap resizedImage = FTVImageProcEngine.imageResize(originImage, StringUtil.randomFilename(), true);
 
-            // TODO: execute API in sync mode, call NEC stuff
+            // execute API in sync mode, call NEC stuff
+            String brand_slug = FTVImageProcEngine.executeApi(this, resizedImage);
+            Log.d(TAG, String.format("brand slug : %s", brand_slug));
 
-            // TODO: image post to our server
+            if (brand_slug.isEmpty()) {
+                brand_slug = "GUCCI";
+            }
 
-            // TODO: show webview activity
+            // image post to our server
+            FTVImageProcEngine.postData(resizedImage, brand_slug);
 
+            // show webview activity
             Intent is = new Intent(this, FTVWebViewActivity.class);
             startActivity(is);
-//			DamyGaziring damyGaziring = new DamyGaziring("http://fashiontv.co.jp", this);
-            // damyGaziring.
 		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
