84a0844d7706c68192e72dde166738c845e11fda
Alsor Zhou <alsor.zhou@gmail.com>
2013-11-11 21:24:09 +0800

    android - add device util functions

---
+++ Android/fscan/src/jp/co/fashiontv/fscan/Common/DeviceUtil.java
@@ -0,0 +1,66 @@
+package jp.co.fashiontv.fscan.Common;
+
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import jp.co.fashiontv.fscan.FTVApplication;
+import jp.co.fashiontv.fscan.R;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Alsor Zhou on 13-11-10.
+ */
+public class DeviceUtil {
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    /** Create a file Uri for saving an image or video */
+    public static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    public static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES), FTVApplication.getContext().getString(R.string.app_name));
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    public static String photoDirectory(){
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) +
+            "/" + FTVApplication.getContext().getString(R.string.app_name);
+
+        return path;
+    }
+}
