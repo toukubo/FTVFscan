b75f9fecb9367e397d465973f664db0f1ce7eba8
koji toukubo <toukubo@gmail.com>
2013-11-13 03:37:30 +0900

    just cut dirty japanese strings.

--- Android/fscan/src/jp/co/fashiontv/fscan/SplashActivity.java
+++ Android/fscan/src/jp/co/fashiontv/fscan/SplashActivity.java
@@ -7,25 +7,21 @@ import android.os.Handler;
 import android.view.Window;
 
 public class SplashActivity extends Activity {
-	private static final int SPLASHTIME = 3;
+	private static final int SPLASHTIME = 5;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// タイトルを非表示にします。
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		// splash.xmlをViewに指定します。
 		setContentView(R.layout.activity_splash);
 		Handler hdl = new Handler();
-		// 500ms遅延させてsplashHandlerを実行します。
 		hdl.postDelayed(new splashHandler(), SPLASHTIME * 1000);
 	}
 	class splashHandler implements Runnable {
 		public void run() {
-			// スプラッシュ完了後に実行するActivityを指定します。
 			Intent intent = new Intent(getApplication(), MainActivity.class);
 			startActivity(intent);
-			// SplashActivityを終了させます。
 			SplashActivity.this.finish();
 		}
 	}
