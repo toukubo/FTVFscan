b774993c1da43b3320c7037dd55d9ed680c6a5f0
Lihui Wang <wanglihui@zbdang.com>
2013-11-23 16:38:19 +0800

    load custom url, native vc switch within app

--- FTVFscan/FTVDelayJobWebViewController.m
+++ FTVFscan/FTVDelayJobWebViewController.m
@@ -107,12 +107,14 @@
 {
     NSString *urlString = request.URL.absoluteString;
 //    DLog(@"%@", urlString);
-    if ([self needOpenExternalSafari:urlString]) {
-        [FTVImageProcEngine openSafari:urlString];
-        return NO;
-    }
+//    if ([self needOpenExternalSafari:urlString]) {
+//        [FTVImageProcEngine openSafari:urlString];
+//        return NO;
+//    }
+//    
+//    return YES;
     
-    return YES;
+    return ![self shouldOverrideUrlLoading:urlString];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
@@ -141,4 +143,86 @@
     
     return NO;
 }
+
+- (BOOL)shouldOverrideUrlLoading:(NSString *)urlString
+{
+    if ([self needOpenExternalSafari:urlString]) {
+        [FTVImageProcEngine openSafari:urlString];
+        return YES;
+    }
+    else if ([urlString hasPrefix:@"inapp-http"]) {
+        NSString *uri = [urlString stringByReplacingOccurrencesOfString:@"inapp-http://" withString:@""];
+        if ([uri hasPrefix:@"local/"]) {
+            uri = [uri stringByReplacingOccurrencesOfString:@"local/" withString:@""];
+            [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"file:///android_asset/" ofType:@"*"] isDirectory:NO]]];
+        }
+        else
+        {
+            /** @TODO this code, is NOT tested and being commited. */
+            if([urlString rangeOfString:@"scan/list.php"].location != NSNotFound)
+            {
+                // Todo by gailya, what's the device id;
+                [self loadUrl:[NSString stringWithFormat:@"http://%@?deviceid=%d", uri, 999]];
+            
+            }
+            else
+            {
+                [self loadUrl:[NSString stringWithFormat:@"http://%@", uri]];
+            }
+        }
+        return YES;
+    }
+    else if([urlString hasSuffix:@".action"])
+    {
+        NSString *action = [urlString stringByReplacingOccurrencesOfString:@".action" withString:@""];
+        
+        // Todo: gailya need to confirm the way to load the camera/gallery with drawer
+        if ([action isEqualToString:@"Camera"]) {
+            DDMenuController *menuController = (DDMenuController*)((FTVAppDelegate *)[[UIApplication sharedApplication] delegate]).menuController;
+            UIViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"FTVCameraViewController"];
+            [menuController setRootController:controller animated:YES];
+        }
+        else if([action isEqualToString:@"Gallery"])
+        {
+            DDMenuController *menuController = (DDMenuController*)((FTVAppDelegate *)[[UIApplication sharedApplication] delegate]).menuController;
+            UIViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"FTVGalleryViewController"];
+            [menuController setRootController:controller animated:YES];
+        }
+        
+        return YES;
+    }
+                 // TODO gailya, handle this later
+//        else if (url.contains(".ahtml")) {
+//        URL urlObject = null;
+//        try {
+//            urlObject = new URL(url);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        InputStream is = null;
+//        try {
+//            is = urlObject.openStream();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String thehtml = null;
+//        try {
+//            thehtml = IOUtils.toString(is);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        
+//        for (Iterator iterator = this.attributeSet.keySet().iterator(); iterator.hasNext(); ) {
+//            String key = (String) iterator.next();
+//            String value = this.attributeSet.get(key);
+//            thehtml = thehtml.replaceAll("\\$\\{" + key + "\\}", value);
+//        }
+//        view.loadDataWithBaseURL("file:///android_asset/", thehtml, "text/html", "UTF-8", null);
+//        
+//        return YES;
+//        
+//    }
+    return NO;
+}
+
 @end
