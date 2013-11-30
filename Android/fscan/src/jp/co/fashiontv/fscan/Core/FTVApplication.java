00576c5c8c7bd4ec38d9c28716e5da9ae7155e0a
Alsor Zhou <alsor.zhou@gmail.com>
2013-11-30 12:24:02 +0800

    android - import testflight app support

--- Android/fscan/src/jp/co/fashiontv/fscan/Core/FTVApplication.java
+++ Android/fscan/src/jp/co/fashiontv/fscan/Core/FTVApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.view.Display;
 import android.view.WindowManager;
+import com.testflightapp.lib.TestFlight;
 
 /**
  * Created by Alsor Zhou on 13-11-9.
@@ -34,5 +35,14 @@ public class FTVApplication extends Application {
             display = wm.getDefaultDisplay();
         }
         return display;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize TestFlight with your app token.
+        // https://www.testflightapp.com/dashboard/applications/907671/token/
+        TestFlight.takeOff(this, "a6693e1d-95e8-495b-a7c3-ff6e5bd7f2f8");
     }
 }
