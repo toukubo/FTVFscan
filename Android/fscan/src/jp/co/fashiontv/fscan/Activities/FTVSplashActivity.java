5292a4f06200442e007edaedaa4b0dbc28c1a776
ashu_ims <ashu.ims09@gmail.com>
2014-01-17 19:55:25 +0530

    Added bugsense

--- Android/fscan/src/jp/co/fashiontv/fscan/Activities/FTVSplashActivity.java
+++ Android/fscan/src/jp/co/fashiontv/fscan/Activities/FTVSplashActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+
+import com.bugsense.trace.BugSenseHandler;
 import com.testflightapp.lib.TestFlight;
 import jp.co.fashiontv.fscan.Common.FTVConstants;
 import jp.co.fashiontv.fscan.R;
@@ -17,6 +19,9 @@ public class FTVSplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// code for adding bugsence
+		BugSenseHandler.initAndStartSession(FTVSplashActivity.this, "9ba8ee51");
 		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
