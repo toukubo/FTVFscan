854ba1a8cf59c95e69c111d5ce0157d1b0c36c8a
Dhrupal <sp.bde@sptechnolab.com>
2014-02-18 12:25:39 +0530

    Update ResultActivity
    Put Code For Camera Button

--- Android/fscan/src/jp/co/fashiontv/fscan/Activities/ResultActivity.java
+++ Android/fscan/src/jp/co/fashiontv/fscan/Activities/ResultActivity.java
@@ -6,68 +6,98 @@
 package jp.co.fashiontv.fscan.Activities;
 
 import jp.co.fashiontv.fscan.R;
+import jp.co.fashiontv.fscan.Camera.CameraActivity;
+import jp.co.fashiontv.fscan.Common.FTVConstants;
 import jp.co.fashiontv.fscan.Utils.ImageUtil;
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.testflightapp.lib.TestFlight;
+
 public class ResultActivity extends Activity {
-    /** クエリ画像 */
-    private Bitmap queryImageBMP;
+	/** クエリ画像 */
+	private Bitmap queryImageBMP;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // プレビュー画面レイアウトの登録
-        setContentView(R.layout.result);
-//        ActionBar actionbar = getActionBar();
-//        if (actionbar != null) {
-//            actionbar.setTitle(R.string.title_actionbar_result);
-//        }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// プレビュー画面レイアウトの登録
+		setContentView(R.layout.result);
 
-        // Viewを初期化
-        ImageView iv = (ImageView)findViewById(R.id.result_image);
-        iv.setImageBitmap(null);
-        TextView tv = (TextView)findViewById(R.id.image_path);
-        tv.setText("");
+		// Viewを初期化
+		ImageView iv = (ImageView) findViewById(R.id.result_image);
+		iv.setImageBitmap(null);
+		TextView tv = (TextView) findViewById(R.id.image_path);
+		tv.setText("");
 
-    }
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
+	@Override
+	public void onResume() {
+		super.onResume();
 
-        // クエリ画像を取得
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        if (bundle != null) {
-            queryImageBMP = (Bitmap) bundle.get("imageBitmap");
-        }
+		// クエリ画像を取得
+		Intent intent = getIntent();
+		Bundle bundle = intent.getExtras();
+		if (bundle != null) {
+			queryImageBMP = (Bitmap) bundle.get("imageBitmap");
+		}
 
-        ImageView iv = (ImageView)findViewById(R.id.result_image);
-        iv.setImageBitmap(queryImageBMP);
+		// initialize Ui
+		InitUi();
 
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                // 画像を保存
-                String filePath = ImageUtil.saveImageSD(getApplicationContext(), queryImageBMP);
-                return filePath;
-            }
-            @Override
-            protected void onPostExecute(String filePath) {
-                // ファイル保存先を画面に表示
-                if (queryImageBMP != null && filePath != null) {
-                    TextView tv = (TextView)findViewById(R.id.image_path);
-                    tv.setText(getString(R.string.result_text_image_path) + filePath);
-                }
-            }
-        }.execute();
-    }
+		ImageView iv = (ImageView) findViewById(R.id.result_image);
+		iv.setImageBitmap(queryImageBMP);
+
+		new AsyncTask<Void, Void, String>() {
+			@Override
+			protected String doInBackground(Void... params) {
+				// 画像を保存
+				String filePath = ImageUtil.saveImageSD(
+						getApplicationContext(), queryImageBMP);
+				return filePath;
+			}
+
+			@Override
+			protected void onPostExecute(String filePath) {
+				// ファイル保存先を画面に表示
+				if (queryImageBMP != null && filePath != null) {
+					TextView tv = (TextView) findViewById(R.id.image_path);
+					tv.setText(getString(R.string.result_text_image_path)
+							+ filePath);
+				}
+			}
+		}.execute();
+	}
+
+	private void InitUi() {
+		// TODO Auto-generated method stub
+
+		ImageView ivCamera = (ImageView) findViewById(R.id.camera);
+		ImageView home = (ImageView) findViewById(R.id.home);
+		ImageView slider = (ImageView) findViewById(R.id.slider);
+		home.setVisibility(View.GONE);
+		slider.setVisibility(View.GONE);	
+		ivCamera.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivityCamera();
+			}
+		});
+	}
+
+	public void startActivityCamera() {
+		TestFlight.passCheckpoint("FTVMainActivity - startActivityCamera");
+
+		Intent intent = new Intent(this, CameraActivity.class);
+		startActivityForResult(intent, FTVConstants.activityRequestCodeCamera);
+	}
 
 }
